0d879dedcb222d71706d4e14440ce7dbd081b5f7
tao54tao <steventheer@gmail.com>
2019-04-12 09:58:09 -0400

    Updates

---
+++ precache-manifest.ce9ad4d1b153c538ca83cfba26a7aa70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02bd88ca77b3b2507e90",
+    "url": "/foodi/static/js/runtime~main.a24f2772.js"
+  },
+  {
+    "revision": "3bbc320251a602b126ef",
+    "url": "/foodi/static/js/main.85a8c502.chunk.js"
+  },
+  {
+    "revision": "28eaa0b3533fda34733c",
+    "url": "/foodi/static/js/2.3e2346d1.chunk.js"
+  },
+  {
+    "revision": "3bbc320251a602b126ef",
+    "url": "/foodi/static/css/main.a9289281.chunk.css"
+  },
+  {
+    "revision": "9978049b06ce8cdb9cf1216ae6a9235e",
+    "url": "/foodi/index.html"
+  }
+];
