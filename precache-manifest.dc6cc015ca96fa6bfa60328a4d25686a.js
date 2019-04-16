01bd4c51121c00dd7195c8dccb446054881757f9
tao54tao <steventheer@gmail.com>
2019-04-16 16:47:59 -0400

    Updates

--- precache-manifest.dc6cc015ca96fa6bfa60328a4d25686a.js
+++ precache-manifest.dc6cc015ca96fa6bfa60328a4d25686a.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/foodi/static/js/runtime~main.a24f2772.js"
   },
   {
-    "revision": "be4bd6f9c32616d170b6",
-    "url": "/foodi/static/js/main.3ef9cff7.chunk.js"
+    "revision": "b31fea1f7f4963b332c5",
+    "url": "/foodi/static/js/main.b3489027.chunk.js"
   },
   {
     "revision": "eade38364d23a8d35931",
     "url": "/foodi/static/js/2.0c0a3c9d.chunk.js"
   },
   {
-    "revision": "be4bd6f9c32616d170b6",
+    "revision": "b31fea1f7f4963b332c5",
     "url": "/foodi/static/css/main.94a78580.chunk.css"
   },
   {
-    "revision": "6ab1c97778a205fdd2cad6ad0aea8784",
+    "revision": "e593a03f1286889809650e5d048a76ec",
     "url": "/foodi/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/foodi/precache-manifest.891894f01f16c662c0bb2b756d7c3197.js"
+  "/foodi/precache-manifest.dc6cc015ca96fa6bfa60328a4d25686a.js"
 );
 
 workbox.clientsClaim();
