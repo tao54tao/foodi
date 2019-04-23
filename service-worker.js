1fe59080ab049df76c47f7de8de495eb1109ec29
tao54tao <steventheer@gmail.com>
2019-04-23 17:51:04 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/foodi/precache-manifest.c495caa80d61bd18b0157f30d6cb418c.js"
+  "/foodi/precache-manifest.8e449bc66218e84168f4fe7f0a04dd10.js"
 );
 
 workbox.clientsClaim();
