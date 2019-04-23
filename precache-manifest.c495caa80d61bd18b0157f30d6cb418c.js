10f4eb8291fbf1f3a8ddd8592299cbd2c13f066b
tao54tao <steventheer@gmail.com>
2019-04-23 16:47:45 -0400

    Updates

---
+++ precache-manifest.c495caa80d61bd18b0157f30d6cb418c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02bd88ca77b3b2507e90",
+    "url": "/foodi/static/js/runtime~main.a24f2772.js"
+  },
+  {
+    "revision": "9cc64a7ff0d484e101c9",
+    "url": "/foodi/static/js/main.7ecc023e.chunk.js"
+  },
+  {
+    "revision": "5dba41442f771b7c5596",
+    "url": "/foodi/static/js/2.51ecfe4a.chunk.js"
+  },
+  {
+    "revision": "9cc64a7ff0d484e101c9",
+    "url": "/foodi/static/css/main.f2b321bb.chunk.css"
+  },
+  {
+    "revision": "c6b5bd5860bfd33f1374a64296c2bb48",
+    "url": "/foodi/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/foodi/precache-manifest.739561764bd0dfe962b444506aa2d5d7.js"
+  "/foodi/precache-manifest.c495caa80d61bd18b0157f30d6cb418c.js"
 );
 
 workbox.clientsClaim();
