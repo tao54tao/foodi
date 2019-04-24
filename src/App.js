610f55f485efcf9fc1c3262b2cde180ddc84c35c
tao54tao <steventheer@gmail.com>
2019-04-24 10:36:55 -0400

    move data and functions to App component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,112 @@ import ShoppingHome from './components/Shopping/ShoppingHome';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFetching: false,
+      listGroup: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  // function to make new list in component state.  takes in ListName and ItemList array as arguments
+  addList = (ListName="New Shopping List", ItemList=[]) => {
+    var listGroup = this.state.listGroup;
+    listGroup.push({ListName, ItemList});
+    this.setState({listGroup: listGroup});      
+    
+  }
+
+  // function to remove list.  takes in ListKey as argument. 
+
+  removeList = (ListKey) => {
+    var listGroup = this.state.listGroup;
+    listGroup.splice(ListKey, 1);
+    this.setState({listGroup: listGroup});
+  }
+
+  // function to remove item from specified list in state.  takes in ListKey and ItemKey as arguments
+  removeItem = (ListKey, ItemKey) => {
+
+    var listGroup = this.state.listGroup;
+    var List = this.state.listGroup[ListKey].ItemList;
+    List.splice(ItemKey, 1);
+    listGroup[ListKey].ItemList = List; 
+    this.setState({listGroup: listGroup});
+  }
+
+  // function to add item from specified list in state.  takes in ListKey, ListItem as arguments
+  // ListItem has these props:  item.name, item.quantity, item.type, item.done
+
+  addItem = (ListKey, ListItem={quantity: "", type: ""}) => {
+    var listGroup = this.state.listGroup;
+    var List = this.state.listGroup[ListKey].ItemList;
+    List.unshift({
+      name: ListItem.name,
+      quantity: ListItem.quantity,
+      type: ListItem.type,
+      done: false
+    });
+    listGroup[ListKey].ItemList = List;
+    this.setState({listGroup: listGroup});
+  }
+
+  // function to mark an item as done.  takes in ListKey, ItemKey as arguments
+
+  markItemDone = (ListKey, ItemKey) => {
+
+    var listGroup = this.state.listGroup;
+    var List = this.state.listGroup[ListKey].ItemList;
+    var item = List[ItemKey];
+    item.done = !item.done;
+    List[ItemKey] = item;
+    listGroup[ListKey].ItemList = List;
+    this.setState({listGroup: listGroup});
+  }
+
+
+
+  fetchData = () => {
+    this.setState({...this.state, isFetching: true})
+
+
+     // this is where fetch command would run 
+    
+    {/* https://code.tutsplus.com/tutorials/fetching-data-in-your-react-application--cms-30670 
+      fetch(QUOTE_SERVICE_URL)
+    .then(response => response.json())
+    .then(result => this.setState({quotes: result, 
+                                   isFetching: false}))
+    .catch(e => console.log(e)); */}
+
+    var ListName1 = "Trader Joes";
+    var ItemList1 = [];
+    ItemList1.push({name: "milk", quantity: 1, type: "gal", done: false});
+    ItemList1.push({name: "cheese", quantity: 2, type: "cup", done: true});
+    ItemList1.push({name: "wheat bread", quantity: 1, type: "", done: true});
+    
+  
+    var ListName2 = "Shaws";
+    var ItemList2 = [];
+    ItemList2.push({name: "soda", quantity: 2, type: "L", done: false});
+    ItemList2.push({name: "sour worms", quantity: 1, type: "", done: false});
+    ItemList2.push({name: "ground beef", quantity: 1, type: "lb", done: true});
+
+    this.addList(ListName1,ItemList1);
+    this.addList(ListName2,ItemList2);    
+    this.setState({isFetching: false});
+
+    }
+
+
+
+   
+
+
   render() {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme()}>
@@ -16,7 +122,16 @@ class App extends Component {
       
       <Switch>
         <Route exact path='/' component={Home}/>
-        <Route path='/Shopping' component={ShoppingHome}/>
+        <Route path='/Shopping' 
+        render={(props) => <ShoppingHome {...props} 
+        listGroup={this.state.listGroup}
+        listDel={this.removeList} 
+        listAdd={this.addList}
+        itemAdd={this.addItem} 
+        itemDel={this.removeItem} 
+        itemDone={this.markItemDone}
+        />}
+        />
         <Route component={() => (<div>404 Not found </div>)}/>
         </Switch>
       </div>
--- src/components/Shopping/ShoppingHome.js
+++ src/components/Shopping/ShoppingHome.js
@@ -7,125 +7,20 @@ import './ShoppingHome.css';
 
 
 class ShoppingHome extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
-    this.state = {
-      isFetching: false,
-      listGroup: [],
-    };
+
   }
-
-  componentDidMount() {
-    this.fetchData()
-  }
-
-  // function to make new list in component state.  takes in ListName and ItemList array as arguments
-  addList = (ListName="New Shopping List", ItemList=[]) => {
-    var listGroup = this.state.listGroup;
-    listGroup.push({ListName, ItemList});
-    this.setState({listGroup: listGroup});      
-    
-  }
-
-  // function to remove list.  takes in ListKey as argument. 
-  // not tested or implemented
-
-  removeList = (ListKey) => {
-    var listGroup = this.state.listGroup;
-    listGroup.splice(ListKey, 1);
-    this.setState({listGroup: listGroup});
-  }
-
-  // function to remove item from specified list in state.  takes in ListKey and ItemKey as arguments
-  // not tested yet or implemented
-  removeItem = (ListKey, ItemKey) => {
-
-    var listGroup = this.state.listGroup;
-    var List = this.state.listGroup[ListKey].ItemList;
-    List.splice(ItemKey, 1);
-    listGroup[ListKey].ItemList = List; 
-    this.setState({listGroup: listGroup});
-  }
-
-  // function to add item from specified list in state.  takes in ListKey, ListItem as arguments
-  // ListItem has these props:  item.name, item.quantity, item.type, item.done
-  // not tested yet or implemented
-
-  addItem = (ListKey, ListItem={quantity: "", type: ""}) => {
-    var listGroup = this.state.listGroup;
-    var List = this.state.listGroup[ListKey].ItemList;
-    List.unshift({
-      name: ListItem.name,
-      quantity: ListItem.quantity,
-      type: ListItem.type,
-      done: false
-    });
-    listGroup[ListKey].ItemList = List;
-    this.setState({listGroup: listGroup});
-  }
-
-  // function to mark an item as done.  takes in ListKey, ItemKey as arguments
-  // not tested yet or implemented
-
-  markItemDone = (ListKey, ItemKey) => {
-
-    var listGroup = this.state.listGroup;
-    var List = this.state.listGroup[ListKey].ItemList;
-    var item = List[ItemKey];
-    item.done = !item.done;
-    List[ItemKey] = item;
-    listGroup[ListKey].ItemList = List;
-    this.setState({listGroup: listGroup});
-  }
-
-
-
-  fetchData = () => {
-    this.setState({...this.state, isFetching: true})
-
-
-     // this is where fetch command would run 
-    
-    {/* https://code.tutsplus.com/tutorials/fetching-data-in-your-react-application--cms-30670 
-      fetch(QUOTE_SERVICE_URL)
-    .then(response => response.json())
-    .then(result => this.setState({quotes: result, 
-                                   isFetching: false}))
-    .catch(e => console.log(e)); */}
-
-    var ListName1 = "Trader Joes";
-    var ItemList1 = [];
-    ItemList1.push({name: "milk", quantity: 1, type: "gal", done: false});
-    ItemList1.push({name: "cheese", quantity: 2, type: "cup", done: true});
-    ItemList1.push({name: "wheat bread", quantity: 1, type: "", done: true});
-    
-  
-    var ListName2 = "Shaws";
-    var ItemList2 = [];
-    ItemList2.push({name: "soda", quantity: 2, type: "L", done: false});
-    ItemList2.push({name: "sour worms", quantity: 1, type: "", done: false});
-    ItemList2.push({name: "ground beef", quantity: 1, type: "lb", done: true});
-
-    this.addList(ListName1,ItemList1);
-    this.addList(ListName2,ItemList2);    
-    this.setState({isFetching: false});
-
-    }
-
-
-
-
-
     
     render() {
 
-    var ShoppingListArray = this.state.listGroup.map((list, index) => {
+    var ShoppingListArray = this.props.listGroup.map((list, index) => {
       
       return ( <ShoppingList index={index} ListName={list.ListName} ItemList={list.ItemList} 
-        listDel={this.removeList} 
-        itemAdd={this.addItem} 
-        itemDel={this.removeItem} 
-        itemDone={this.markItemDone} />
+        listDel={this.props.listDel} 
+        itemAdd={this.props.itemAdd} 
+        itemDel={this.props.itemDel} 
+        itemDone={this.props.itemDone} />
         );
     });
 
@@ -134,7 +29,7 @@ class ShoppingHome extends React.Component {
         
         
         <div>
-        <TopMenuBar title="Foodi - Shopping Lists" addButton={this.addList} />
+        <TopMenuBar title="Foodi - Shopping Lists" addButton={this.props.listAdd} />
         <div className="MainWindow">
         <Grid container direction='row' justify='space-evenly' alignItems='flex-start' spacing={16}>
 
@@ -144,16 +39,11 @@ class ShoppingHome extends React.Component {
         </div>
         </div>
         
-        
-        
+               
         
       );
-
       
     }
-
-
-
 
 
   }
