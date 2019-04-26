0fc6a2d34368203666aab2dc6e2d7cd3a903132e
tao54tao <steventheer@gmail.com>
2019-04-26 12:58:27 -0400

    default page when no lists

--- src/components/Shopping/ShoppingHome.js
+++ src/components/Shopping/ShoppingHome.js
@@ -4,6 +4,38 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import TopMenuBar from '../TopMenuBar/TopMenuBar';
 import './ShoppingHome.css';
+import { withStyles } from '@material-ui/core/styles';
+import centerFocusStrong from 'material-ui/svg-icons/image/center-focus-strong';
+
+const styles = theme => ({
+
+  instructionsGrid : {
+    marginRight: theme.spacing.unit * 2,
+    marginLeft: theme.spacing.unit * 2,
+    color: "rgba(0, 0, 0, 0.87)",
+    width: '100%',
+    padding: '0',
+  },
+
+  notFound: {
+    flexGrow: 1,
+    minWidth: 0,
+    textAlign: 'center',
+  
+    
+  },
+
+  FullWidth : {
+    width: '100%',
+  },
+
+
+
+  
+
+
+
+});
 
 
 class ShoppingHome extends React.Component {
@@ -13,22 +45,42 @@ class ShoppingHome extends React.Component {
   }
     
     render() {
+      const { classes } = this.props;
 
-    var ShoppingListArray = this.props.listGroup.map((list, index) => {
+    if (this.props.listGroup.length == 0) {
+    var ShoppingListArray = <Grid container item justify="center" wrap="nowrap" direction="column" className={classes.instructionsGrid}>
+    
+  
+    <Grid item className={classes.notFound}>
+    <Typography variant="h6" color="inherit">No Lists Found</Typography>
+    </Grid>
+    
+    </Grid>
       
-      return ( <ShoppingList 
-        index={index} 
-        ListName={list.ListName} 
-        ItemList={list.ItemList} 
-        listDel={this.props.listDel} 
-        setListName={this.props.setListName}
-        itemAdd={this.props.itemAdd} 
-        itemDel={this.props.itemDel} 
-        itemDone={this.props.itemDone}
+    }
+    else {
+      var ShoppingListArray = this.props.listGroup.map((list, index) => {
+        return ( <ShoppingList 
+          index={index} 
+          ListName={list.ListName} 
+          ItemList={list.ItemList} 
+          listDel={this.props.listDel} 
+          setListName={this.props.setListName}
+          itemAdd={this.props.itemAdd} 
+          itemDel={this.props.itemDel} 
+          itemDone={this.props.itemDone}
+  
+          />
+          );
 
-        />
-        );
-    });
+
+      });
+      
+    };
+      
+
+
+    
 
     
       return (
@@ -54,4 +106,4 @@ class ShoppingHome extends React.Component {
 
   }
   
-  export default ShoppingHome;
+  export default withStyles(styles)(ShoppingHome);
